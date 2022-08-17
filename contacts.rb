7ab9bff7b7671f76d95153127db825d4a6968e7d
Dennis Mwangi <dennis.kamau@student.moringaschool.com>
2022-08-17 07:13:52 +0300

    commit tree a555128fbddd4f1300f76eef92227c8abff30cac

--- contacts.rb
+++ contacts.rb
@@ -15,10 +15,22 @@ def contacts
       favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
   }
-end
 
+ 
+end
+ 
+
+  
 def remove_strawberry(contacts)
-  # your code here!
+  contacts.each do |person,contact_details_hash|
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attribute,data|
+        if attribute == :favorite_ice_cream_flavors
+          data.delete_if {|flavor| flavor == "strawberry"}
+        end
+      end
+    end
+  end
 end
 
 # print the output to the terminal for inspection
